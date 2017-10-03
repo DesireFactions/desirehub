1b92b8a698ca9f4745c87310bf5371f23209d206
Michael Ziluck <ziluckm@iastate.edu>
2017-10-02 20:26:13 -0500

    progress on the menu

--- src/main/java/com/desiremc/hub/DesireHub.java
+++ src/main/java/com/desiremc/hub/DesireHub.java
@@ -1,7 +1,11 @@
 package com.desiremc.hub;
 
+import java.io.File;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.desiremc.core.api.FileHandler;
+import com.desiremc.core.api.LangHandler;
 import com.desiremc.hub.session.ServerHandler;
 
 public class DesireHub extends JavaPlugin
@@ -9,14 +13,20 @@ public class DesireHub extends JavaPlugin
 
     private static DesireHub instance;
 
+    private static FileHandler config;
+    private static LangHandler lang;
+
     @Override
     public void onEnable()
     {
         instance = this;
 
+        config = new FileHandler(new File(getDataFolder(), "config.yml"));
+        lang = new LangHandler(new File(getDataFolder(), "lang.yml"));
+
         registerCommands();
         registerListeners();
-        
+
         ServerHandler.initialize();
     }
 
@@ -28,6 +38,16 @@ public class DesireHub extends JavaPlugin
     private void registerListeners()
     {
 
+    }
+
+    public static FileHandler getConfigHandler()
+    {
+        return config;
+    }
+
+    public static LangHandler getLangHandler()
+    {
+        return lang;
     }
 
     public static DesireHub getInstance()
--- src/main/java/com/desiremc/hub/gui/ServerGUI.java
+++ src/main/java/com/desiremc/hub/gui/ServerGUI.java
@@ -1,6 +1,46 @@
 package com.desiremc.hub.gui;
 
-public class ServerGUI
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import com.desiremc.core.DesireCore;
+import com.desiremc.core.gui.Menu;
+import com.desiremc.core.gui.MenuItem;
+import com.desiremc.hub.DesireHub;
+import com.desiremc.hub.session.Server;
+import com.desiremc.hub.session.ServerHandler;
+
+public class ServerGUI extends Menu
 {
+
+    public ServerGUI()
+    {
+        super(DesireHub.getConfigHandler().getString("inventory.title"), 1);
+
+        loadServers();
+    }
+
+    private void loadServers()
+    {
+        int i = 0;
+        for (Server server : ServerHandler.getInstance().getServers())
+        {
+            addMenuItem(new MenuItem(server.getName(), getItem(server))
+            {
+
+                @Override
+                public void onClick(Player player)
+                {
+                    // TODO redirect them to the right server
+                }
+            }, i);
+            i++;
+        }
+    }
+
+    private ItemStack getItem(Server server)
+    {
+        return new ItemStack(DesireCore.getItemHandler().get(DesireHub.getConfigHandler().getString("servers." + server.getName() + ".item")));
+    }
 
 }
