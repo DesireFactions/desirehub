4dc817eb3d2e77cab814a40ecf7216d24163cbc6
Michael Ziluck <ziluckm@iastate.edu>
2017-10-28 12:59:18 -0500

    Prevent players from breaking/placing blocks and give them food.

--- src/main/java/com/desiremc/hub/listeners/EntityListener.java
+++ src/main/java/com/desiremc/hub/listeners/EntityListener.java
@@ -3,8 +3,11 @@ package com.desiremc.hub.listeners;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 
 public class EntityListener implements Listener
 {
@@ -27,4 +30,26 @@ public class EntityListener implements Listener
         }
     }
 
+    @EventHandler
+    public void onFood(FoodLevelChangeEvent e)
+    {
+        if (e.getFoodLevel() != 20)
+        {
+            e.setFoodLevel(20);
+        }
+        e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e)
+    {
+        e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent e)
+    {
+        e.setCancelled(true);
+    }
+    
 }
