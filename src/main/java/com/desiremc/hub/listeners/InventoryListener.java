dfa315c25c6ec505756d81d8b2e13a63012f1615
BuildTools <unconfigured@null.spigotmc.org>
2017-11-23 02:02:49 -0500

    fix players being able to drop the compass

--- src/main/java/com/desiremc/hub/listeners/InventoryListener.java
+++ src/main/java/com/desiremc/hub/listeners/InventoryListener.java
@@ -3,6 +3,7 @@ package com.desiremc.hub.listeners;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryInteractEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 
 public class InventoryListener implements Listener
 {
@@ -11,6 +12,12 @@ public class InventoryListener implements Listener
     public void onMove(InventoryInteractEvent e)
     {
         e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onItemDrop(PlayerDropItemEvent event)
+    {
+        event.setCancelled(true);
     }
 
 }
