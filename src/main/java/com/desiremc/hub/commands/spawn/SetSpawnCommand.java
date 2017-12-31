c06d9c6af20679b6d1c114402549bad7bf6a440e
ctooley21 <christian.tooley@uky.edu>
2017-12-30 18:38:01 -0600

    Separate spawn commands

---
+++ src/main/java/com/desiremc/hub/commands/spawn/SetSpawnCommand.java
@@ -0,0 +1,41 @@
+package com.desiremc.hub.commands.spawn;
+
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import com.desiremc.core.api.FileHandler;
+import com.desiremc.core.api.newcommands.CommandArgument;
+import com.desiremc.core.api.newcommands.ValidCommand;
+import com.desiremc.core.session.Rank;
+import com.desiremc.core.session.Session;
+import com.desiremc.hub.DesireHub;
+
+public class SetSpawnCommand extends ValidCommand
+{
+
+    public SetSpawnCommand()
+    {
+        super("setspawn", "Set the server spawn.", Rank.ADMIN, true);
+    }
+
+    @Override
+    public void validRun(Session sender, String label[], List<CommandArgument<?>> args)
+    {
+        Player p = sender.getPlayer();
+        FileHandler config = DesireHub.getConfigHandler();
+        Location loc = p.getLocation();
+
+        config.setDouble("spawn.x", loc.getX());
+        config.setDouble("spawn.y", loc.getY());
+        config.setDouble("spawn.z", loc.getZ());
+        config.setDouble("spawn.pitch", loc.getPitch());
+        config.setDouble("spawn.yaw", loc.getYaw());
+        config.setString("spawn.world", loc.getWorld().getName());
+
+        SpawnCommand.getInstance().setSpawn(loc);
+
+        DesireHub.getLangHandler().sendRenderMessage(sender, "spawn.set", true, false);
+    }
+}
---
+++ src/main/java/com/desiremc/hub/commands/spawn/SpawnCommand.java
@@ -0,0 +1,81 @@
+package com.desiremc.hub.commands.spawn;
+
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import com.desiremc.core.api.FileHandler;
+import com.desiremc.core.api.newcommands.CommandArgument;
+import com.desiremc.core.api.newcommands.CommandArgumentBuilder;
+import com.desiremc.core.api.newcommands.ValidCommand;
+import com.desiremc.core.parsers.PlayerParser;
+import com.desiremc.core.session.Rank;
+import com.desiremc.core.session.Session;
+import com.desiremc.hub.DesireHub;
+
+public class SpawnCommand extends ValidCommand
+{
+
+    private Location spawn;
+    private static SpawnCommand instance;
+
+    public SpawnCommand()
+    {
+        super("spawn", "Teleport to the server spawn.", Rank.GUEST, true);
+
+        addArgument(CommandArgumentBuilder.createBuilder(Player.class)
+                .setName("target")
+                .setParser(new PlayerParser())
+                .setOptional()
+                .setRequiredRank(Rank.HELPER)
+                .build());
+
+        instance = this;
+    }
+
+    @Override
+    public void validRun(Session sender, String label[], List<CommandArgument<?>> args)
+    {
+        Player player = args.get(0).hasValue() ? (Player) args.get(0).getValue() : sender.getPlayer();
+
+        player.teleport(getSpawn());
+
+        if (args.get(0).hasValue())
+        {
+            DesireHub.getLangHandler().sendRenderMessage(sender, "spawn.target", true, false,
+                    "{target}", player.getName());
+            DesireHub.getLangHandler().sendRenderMessage(player, "spawn.force", true, false);
+        }
+        else
+        {
+            DesireHub.getLangHandler().sendRenderMessage(sender, "spawn.confirm", true, false);
+        }
+    }
+
+    public static SpawnCommand getInstance()
+    {
+        return instance;
+    }
+
+    public Location getSpawn()
+    {
+        if (spawn == null)
+        {
+            FileHandler config = DesireHub.getConfigHandler();
+            new Location(Bukkit.getWorld(config.getString("spawn.world")),
+                    config.getDouble("spawn.x"),
+                    config.getDouble("spawn.y"),
+                    config.getDouble("spawn.z"),
+                    config.getDouble("spawn.yaw").floatValue(),
+                    config.getDouble("spawn.pitch").floatValue());
+        }
+        return spawn;
+    }
+
+    public void setSpawn(Location spawn)
+    {
+        getInstance().spawn = spawn;
+    }
+}
